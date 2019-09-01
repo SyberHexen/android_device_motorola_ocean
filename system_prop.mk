40a97bd469e53ecaa5771d1f48a2a60a7fa6dc64
SyberHexen <SyberHexen@gmail.com>
2019-08-31 17:07:38 -0700

    river: update props
    * from stock
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -21,10 +21,62 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.min.duration.secs=30
+    audio.deep_buffer.media=true \
+    audio.offload.disable=false \
+    audio.offload.video=false \
+    audio.offload.min.duration.secs=60 \
+    audio.offload.gapless.enabled=false \
+    media.aac_51_output_enabled=true \
+    audio.offload.min.duration.secs=30 \
+    persist.speaker.prot.enable=false \
+    persist.audio.AoU.configFile=river \
+    persist.audio.qti.flac.decoder=true \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+    qcom.hw.aac.encoder=true \
+    ro.af.client_heap_size_kbyte=7168 \
+    voice.playback.conc.disabled=true \
+    voice.record.conc.disabled=false \
+    voice.voip.conc.disabled=true \
+    voice.conc.fallbackpath=deep-buffer
+
+# Audio Snapdragon
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.audio.ssr=false \
+    ro.vendor.audio.sdk.fluencetype=none \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
+    tunnel.audio.encode = false \
+    audio.offload.buffer.size.kb=64 \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    audio.offload.track.enable=true \
+    audio.deep_buffer.media=true \
+    audio.playback.mch.downsample=true \
+    use.voice.path.for.pcm.voip=false \
+    ro.config.vc_call_vol_steps=8 \
+    use.qti.sw.alac.decoder=true \
+    use.qti.sw.ape.decoder=true \
+    audio.offload.gapless.enabled=true \
+    audio.offload.multiple.enabled=false \
+    vendor.audio.safx.pbe.enabled=true \
+    audio.pp.asphere.enabled=false \
+    ro.vendor.gt_library=libqti-gt.so \
+    ro.vendor.at_library=libqti-at.so 
+
+# Encoding
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.vidc.enc.disable_bframes=1 \
+    vendor.vidc.disable.split.mode=1 \
+    vendor.vidc.dec.downscalar_width=1920 \
+    vendor.vidc.dec.downscalar_height=1088 \
+    vendor.video.disable.ubwc=1 \
+    vendor.vidc.enc.disable.pq=true \
+    vidc.enc.dcvs.extra-buff-count=2 
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.enable_default_color_mode=1 \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1
@@ -32,6 +84,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    rild.libargs=-d/dev/smd0 \
+    ril.subscription.types=NV,RUIM \
+    DEVICE_PROVISIONED=1 \
+    persist.radio.fi_supported=1 \
+    telephony.lteOnCdmaDevice=1
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -46,7 +107,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Netmgr
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.use_data_netmgrd=true \
     persist.vendor.data.mode=concurrent
+
+# Time-Services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
