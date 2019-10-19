d89aecb7f503b13b3e2d836bf2447981dc7102b6
Erfan Abdi <erfangplus@gmail.com>
2019-10-19 12:35:14 +0330

    river: Move sensor configs to configs/

--- device.mk
+++ device.mk
@@ -45,8 +45,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Thermal
 PRODUCT_COPY_FILES += \
