20ef42b6f5b0ea4d6b9d195694624ee24b321e4b
Erfan Abdi <erfangplus@gmail.com>
2021-10-13 03:28:21 +0330

    ocean: Switch to AOSP NoCutoutOverlay and define prop
    Change-Id: I39ac6a1d66f32c611a6cc4ce3ddcc1353a438f4d

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    AvoidAppsInCutoutOverlay \
     NoCutoutOverlay
 
 # AAPT
--- properties.mk
+++ properties.mk
@@ -32,3 +32,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.expose.aux=1
 
+# Cutout
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_hide_display_cutout=true
