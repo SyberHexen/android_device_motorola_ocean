d2edc0e725b1c897bdc3b2ce9b220494abe0536d
Andrew Hexen <SyberHexen@gmail.com>
2023-01-04 11:59:39 -0800

    ocean: Import sensors.ssc.so from common
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I1d68417d95d5f6508aab4269da2764d4c90ae605

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        # libutils-v32
+        vendor/lib/sensors.ssc.so | vendor/lib64/sensors.ssc.so)
+            "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
